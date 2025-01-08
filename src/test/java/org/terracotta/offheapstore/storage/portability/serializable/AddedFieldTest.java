53c263ccd6921f3b40b556816e85916f2485a050
Chris Dennis <chris.w.dennis@gmail.com>
2025-01-08 15:30:35 -0500

    Update to direct IBM copyright

--- src/test/java/org/terracotta/offheapstore/storage/portability/serializable/AddedFieldTest.java
+++ src/test/java/org/terracotta/offheapstore/storage/portability/serializable/AddedFieldTest.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2015-2023 Terracotta, Inc., a Software AG company.
- * Copyright Super iPaaS Integration LLC, an IBM Company 2024
+ * Copyright IBM Corp. 2024, 2025
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
